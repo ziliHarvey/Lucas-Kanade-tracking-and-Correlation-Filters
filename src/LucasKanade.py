26b193901f1d7909771a9244e8d9e1c2705466f6
ziliHarvey <zili@andrew.cmu.edu>
2020-05-20 19:30:04 -0400

    also precomputed gradient matrix, and improved tracking frequency from 19 to 28 hz on my local machine

--- src/LucasKanade.py
+++ src/LucasKanade.py
@@ -19,13 +19,12 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
     
     threshold = 0.1
     x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
-    Iy, Ix = np.gradient(It1)
-
     rows_img, cols_img = It.shape
     rows_rect, cols_rect = x2 - x1, y2 - y1
     dp = [[cols_img], [rows_img]] #just an intial value to enforce the loop
 
     # template-related can be precomputed
+    Iy, Ix = np.gradient(It1)
     y = np.arange(0, rows_img, 1)
     x = np.arange(0, cols_img, 1)     
     c = np.linspace(x1, x2, cols_rect)
@@ -33,6 +32,8 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
     cc, rr = np.meshgrid(c, r)
     spline = RectBivariateSpline(y, x, It)
     T = spline.ev(rr, cc)
+    spline_gx = RectBivariateSpline(y, x, Ix)
+    spline_gy = RectBivariateSpline(y, x, Iy)
 
     # in translation model jacobian is not related to coordinates
     jac = np.array([[1,0],[0,1]])
@@ -53,14 +54,10 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
         errImg = err.reshape(-1,1) 
         
         #compute gradient
-        spline_gx = RectBivariateSpline(y, x, Ix)
         Ix_w = spline_gx.ev(rrw, ccw)
-
-        spline_gy = RectBivariateSpline(y, x, Iy)
         Iy_w = spline_gy.ev(rrw, ccw)
         #I is (n,2)
         I = np.vstack((Ix_w.ravel(),Iy_w.ravel())).T
-        
         
         #computer Hessian
         delta = I @ jac 
@@ -74,6 +71,5 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
         #update parameters
         p0[0] += dp[0,0]
         p0[1] += dp[1,0]
-        
-    p = p0
-    return p
+    
+    return p0
