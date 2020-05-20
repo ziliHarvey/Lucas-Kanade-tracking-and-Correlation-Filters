359472884cadf753ababbf7a06ede20ba702d748
ziliHarvey <zili@andrew.cmu.edu>
2020-05-20 10:32:22 -0400

    fixed issue that tracking patches are plotted on a blank image when running in terminal cmdline

--- src/testAerialSequence.py
+++ src/testAerialSequence.py
@@ -14,12 +14,13 @@ for i in range(frames.shape[2]-1):
     if i == 31 or i == 61 or i == 91 or i == 121:
         
         plt.figure()
+        plt.imshow(image1, cmap='gray')
         for r in range(mask.shape[0]):
             for c in range(mask.shape[1]):
                 if mask[r,c] == True:
                     plt.scatter(c, r, s = 10, c = 'b')
-                    plt.show()
-        plt.imshow(image1, cmap='gray')
+        plt.show()
+        
        
         
  
