64d9661c901b200a37d84158c035b588c70a4b70
ziliHarvey <zili@andrew.cmu.edu>
2020-05-20 18:50:53 -0400

    optimized code by pre-computing template and jocobian matrix before entering the iterations

--- src/LucasKanade.py
+++ src/LucasKanade.py
@@ -20,30 +20,31 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
     threshold = 0.1
     x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
     Iy, Ix = np.gradient(It1)
+
     rows_img, cols_img = It.shape
     rows_rect, cols_rect = x2 - x1, y2 - y1
     dp = [[cols_img], [rows_img]] #just an intial value to enforce the loop
 
+    # template-related can be precomputed
+    y = np.arange(0, rows_img, 1)
+    x = np.arange(0, cols_img, 1)     
+    c = np.linspace(x1, x2, cols_rect)
+    r = np.linspace(y1, y2, rows_rect)
+    cc, rr = np.meshgrid(c, r)
+    spline = RectBivariateSpline(y, x, It)
+    T = spline.ev(rr, cc)
+
+    # in translation model jacobian is not related to coordinates
+    jac = np.array([[1,0],[0,1]])
+
     while np.square(dp).sum() > threshold:
             
         # warp image using translation motion model
-
         x1_w, y1_w, x2_w, y2_w = x1+p0[0], y1+p0[1], x2+p0[0], y2+p0[1]
         
-        y = np.arange(0, rows_img, 1)
-        x = np.arange(0, cols_img, 1)
-        
-        c = np.linspace(x1, x2, cols_rect)
-        r = np.linspace(y1, y2, rows_rect)
-        cc, rr = np.meshgrid(c, r)
-    
         cw = np.linspace(x1_w, x2_w, cols_rect)
         rw = np.linspace(y1_w, y2_w, rows_rect)
         ccw, rrw = np.meshgrid(cw, rw)
-
-        spline = RectBivariateSpline(y, x, It)
-        T = spline.ev(rr, cc)
-        
         spline1 = RectBivariateSpline(y, x, It1)
         warpImg = spline1.ev(rrw, ccw)
         
@@ -60,8 +61,6 @@ def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
         #I is (n,2)
         I = np.vstack((Ix_w.ravel(),Iy_w.ravel())).T
         
-        #evaluate jacobian (2,2)
-        jac = np.array([[1,0],[0,1]])
         
         #computer Hessian
         delta = I @ jac 
