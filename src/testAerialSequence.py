60845fec2896f0eca2030721014e2b5a2b9cb13d
ziliHarvey <zili@andrew.cmu.edu>
2020-05-20 22:35:56 -0400

    added timing for aerial sequence

--- src/testAerialSequence.py
+++ src/testAerialSequence.py
@@ -4,12 +4,23 @@ from matplotlib import animation
 import matplotlib.patches as patches
 from SubtractDominantMotion import *
 from LucasKanadeAffine import *
+import time
 
 frames = np.load('../data/aerialseq.npy')
-for i in range(frames.shape[2]-1):
-    image1 = frames[:, :, i]
-    image2 = frames[:, :, i+1]
+time_total = 0
+seq_len = frames.shape[2]
+
+for i in range(seq_len):
+    if i == 0:
+        continue
+    print("Processing frame %d" % i)
+
+    start = time.time()
+    image1 = frames[:, :, i-1]
+    image2 = frames[:, :, i]
     mask = SubtractDominantMotion(image1, image2)
+    end = time.time()
+    time_total += end - start
 
     if i == 31 or i == 61 or i == 91 or i == 121:    
         plt.figure()
@@ -19,3 +30,5 @@ for i in range(frames.shape[2]-1):
                 if mask[r,c]:
                     plt.scatter(c, r, s = 1, c = 'r', alpha=0.5)
         plt.show()
+
+print('Finished, the tracking frequency is %.4f' % (seq_len / time_total))
